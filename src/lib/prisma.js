ba26bc12ca741f378d86fe6bacb6c6b459f1ee5b
lFobas <mishashuptar@gmail.com>
2024-06-05 13:10:11 +0300

    prisma client error

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,13 +1,20 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+
+
+// import { PrismaClient } from '@prisma/client';
+
+
+// if (process.env.NODE_ENV === 'production') {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+//   prisma = global.prisma;
+// }
 
 export default prisma;
