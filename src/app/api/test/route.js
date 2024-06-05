09444a7680e90898c9c4ca29a40378713aa4cc9a
lFobas <mishashuptar@gmail.com>
2024-06-05 12:53:02 +0300

    triger2

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,9 +1,7 @@
-import { nar } from "@/lib/nar";
-import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function POST (req, res){
-    const data = nar
+    
     //const cli = await prisma.record.createMany({data})
     // const cli = await prisma.record.create({data:{
     //     date: "2021-08-11T00:00:00Z",
