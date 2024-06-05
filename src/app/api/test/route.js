5b3a6e4975ef206cb165099bb32663d496782576
lFobas <mishashuptar@gmail.com>
2024-06-05 12:50:34 +0300

    triger

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -9,7 +9,7 @@ export async function POST (req, res){
     //     date: "2021-08-11T00:00:00Z",
     //     summa: 123,
     //     authorId: "Лакатош Пейтер",
-    //     description :"лакотош й 324                                         Дерцен -  -                                       149                                               "
+    //     description :"лакотош й 324 Дерцен -  - 149                                               "
 
     // }})
     return NextResponse.json({ cli });
