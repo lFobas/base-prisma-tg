7fac214c9e43b27694ffb1bb056510788692468e
lFobas <mishashuptar@gmail.com>
2024-06-08 18:05:47 +0300

    revorl validate 3

--- src/app/page.js
+++ src/app/page.js
@@ -1,50 +1,13 @@
 import Table from "@/components/Table";
-import prisma from "@/lib/prisma";
+import { getAdreses, getData } from "@/lib/fetchData";
 
 
-async function getData(){
-  const data = await prisma.client.findMany({include: {
-    records: true,
-    adres: true,
-  },})
-  
 
-  const newData = data.map(item => ({
-    ...item,
-    records: item.records.map(record => ({
-      ...record,
-      summa: Number(record.summa)
-    }))
-  }));
-  newData.sort((a, b) => {
-    const homeA = parseInt(a?.home, 10);
-    const homeB = parseInt(b?.home, 10);
-    
-    if (homeA < homeB) {
-      return -1;
-      }
-    if (homeA > homeB) {
-        return 1;
-      }
-    const streetA = a.street?.toLowerCase(); 
-    const streetB = b.street?.toLowerCase();
-  
-    if (streetA < streetB) {
-      return -1; 
-    }
-    if (streetA > streetB) {
-      return 1; 
-    }
-    return 0; 
-  }
-)
-  return newData;
-}
 
 export default async function Home() {
 
-  const data = await getData()
-  const adreses = await prisma.adres.findMany({  })
+  const data = await getData();
+  const adreses = await getAdreses();
 
   return (
     <main className="min-h-screen flex">  
---
+++ src/lib/fetchData.js
@@ -0,0 +1,48 @@
+import prisma from "./prisma";
+
+
+export async function getData() {
+    console.log('triger user');
+    const data = await prisma.client.findMany({include: {
+        records: true,
+        adres: true,
+      },})
+      
+    
+      const newData = data.map(item => ({
+        ...item,
+        records: item.records.map(record => ({
+          ...record,
+          summa: Number(record.summa)
+        }))
+      }));
+      newData.sort((a, b) => {
+        const homeA = parseInt(a?.home, 10);
+        const homeB = parseInt(b?.home, 10);
+        
+        if (homeA < homeB) {
+          return -1;
+          }
+        if (homeA > homeB) {
+            return 1;
+          }
+        const streetA = a.street?.toLowerCase(); 
+        const streetB = b.street?.toLowerCase();
+      
+        if (streetA < streetB) {
+          return -1; 
+        }
+        if (streetA > streetB) {
+          return 1; 
+        }
+        return 0; 
+      }
+    )
+      return newData;
+    }
+  
+export async function getAdreses() {
+    console.log('triger adres');
+    const adreses = await prisma.adres.findMany();
+    return adreses;
+  }
