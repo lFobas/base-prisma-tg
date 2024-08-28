055244b42f78c591ce5ea8b33a34ad5691ad26e0
lFobas <mishashuptar@gmail.com>
2024-08-28 19:09:38 +0300

    try3

--- src/components/Card/ClientListCard.jsx
+++ src/components/Card/ClientListCard.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 const ClientListCard = ({client, summa}) => {
   return (
-    <Link href={`/client/${client.id}`} className="secondary-text flex-wrap grid grid-cols-8 items-center justify-between border border-gray-300 rounded-md px-1 py-3 my-3 shadow-sm secondary-background w-full">
-      <div className="col-span-1 font-semibold text-gray-800 text-center flex-grow-0">
+    <Link href={`/client/${client.id}`} className="flex-wrap grid grid-cols-8 items-center justify-between border border-gray-300 rounded-md px-1 py-3 my-3 shadow-sm secondary-background w-full">
+      <div className="col-span-1 font-semibold secondary-text text-center flex-grow-0">
         {client.bill}
       </div>
-      <div className="text-gray-800 col-span-6 grid grid-cols-5 border-r-2 border-l-2 mx-1">
+      <div className=" col-span-6 grid grid-cols-5 border-r-2 border-l-2 mx-1 secondary-text ">
        <div className='col-span-2 border-r-2 px-1 text-start'>
         {client.name}
       </div>
