0d9e187a1b961f29c7080c6db7b0f5563d1face2
lFobas <mishashuptar@gmail.com>
2024-09-03 19:04:18 +0300

    some logic of nav bar, edit users state

--- src/components/MenegeMenu.jsx
+++ src/components/MenegeMenu.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react';
-import { UsersIcon, HomeModernIcon } from '@heroicons/react/24/outline';
+import { UsersIcon, HomeModernIcon, MapIcon } from '@heroicons/react/24/outline';
 import UsersTable from './User/UsersTable';
 import { getUsers } from '@/lib/actions';
 import Link from 'next/link';
@@ -10,10 +10,16 @@ export default function MenegeMenu() {
 
   const [content, setContent] = useState(null);
 
-  const handleClick = async () =>{
-    const res = await getUsers()
-    setContent(<UsersTable data={res} />)
-  }
+  const handleClick = async (type) => {
+    if (type === 'users') {
+      const res = await getUsers();
+      setContent(<UsersTable data={res} />);
+    } else if (type === 'adreses') {
+      setContent(<div>adreses Content</div>);
+    } else {
+      setContent(null); 
+    }
+  };
 
   return (
     <div className="flex h-screen">
@@ -32,15 +38,15 @@ export default function MenegeMenu() {
         <nav className="flex-1 p-3 mt-4">
           <ul>
             <li className="mb-4">
-              <button className="flex w-full p-1 button justify-end">
-                <HomeModernIcon className='h-5 w-5' />
-                <span className='ml-3 hidden md:block'>Manage</span>
-              </button>
-            </li>
-            <li className="mb-4">
               <button onClick={() => handleClick('users')} className="flex w-full p-1 justify-end">
                 <UsersIcon className="h-5 w-5" />
                 <span className='ml-3 hidden md:block'>Користувачі</span>
+              </button>
+            </li>
+            <li className="mb-4">
+              <button onClick={() => handleClick('adreses')} className="flex w-full p-1 button justify-end">
+                <MapIcon className='h-5 w-5' />
+                <span className='ml-3 hidden md:block'>Адреси</span>
               </button>
             </li>
           </ul>
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -119,7 +119,7 @@ const Table = ({ adr }) => {
         <div className="flex justify-between">
         <h1 className="text-2xl font-bold"><Link href={"/"}>Борги</Link></h1>
         <div className="flex mt-1">
-          <h2 className="text-end my-auto">{user?.first_name}</h2>
+          <h2 className="text-end my-auto">{user?.name}</h2>
           {/* {user ? <img src={user.photo_url} className="w-10 h-10 mx-1 rounded-full border-2 shadow-lg border-blue-800" /> : null} */}
           <Link className="px-2 py-1 rounded-sm my-bg" href="/manage"> <Cog6ToothIcon className="h-5 w-5" /></Link>
         </div>
