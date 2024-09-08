10e5a310629c787275eb0f958a25595f985478ae
lFobas <mishashuptar@gmail.com>
2024-09-08 20:54:27 +0300

    add loader fetch user data

--- src/app/manage/users/page.jsx
+++ src/app/manage/users/page.jsx
@@ -1,46 +1,53 @@
 'use client'
 import UserCard from '@/components/Card/UserCard';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getUsers } from '@/lib/actions';
 import { useUserStore } from '@/lib/store';
-
+import Spiner from '@/components/Spiner/Spiner';
 
 const UsersPage = () => {
     const { users, setUsers } = useUserStore();
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
       const fetchData = async () => {
         const res = await getUsers();
         setUsers(res);
+        setIsLoading(false);
       };
 
       fetchData();
-    }, [setUsers]); // Оновлення користувачів при зміні
+    }, [setUsers]);
 
     return (
         <div className='sm:w-1/2 mx-auto'>
-          <h1 className='font-semibold'>Admins</h1>
-          {users.filter(u => u.role === 'ADMIN').length > 0 
-            ? users
-                .filter(u => u.role === 'ADMIN')
-                .map((u) => <UserCard key={u.id} user={u} />)
-            : <p className='mx-5'>Немає Адміністраторів</p>}
+          <h1 className='font-semibold'>Admins:</h1>
+          {isLoading
+            ? <Spiner />
+            : users.filter(u => u.role === 'ADMIN').length > 0 
+              ? users
+                  .filter(u => u.role === 'ADMIN')
+                  .map((u) => <UserCard key={u.id} user={u} />)
+              : <p className='mx-5'>Немає Адміністраторів</p>}
 
-          <h1 className='font-semibold'>Users</h1>
-          {users.filter(u => u.role === 'USER').length > 0 
-            ? users
-                .filter(u => u.role === 'USER')
-                .map((u) => <UserCard key={u.id} user={u} />)
-            : <p className='mx-5'>Немає Користувачів</p>}
+          <h1 className='font-semibold'>Users:</h1>
+          {isLoading
+            ? <Spiner />
+            : users.filter(u => u.role === 'USER').length > 0 
+              ? users
+                  .filter(u => u.role === 'USER')
+                  .map((u) => <UserCard key={u.id} user={u} />)
+              : <p className='mx-5'>Немає Користувачів</p>}
 
-          <h1 className='font-semibold'>Unregister</h1>
-          {users.filter(u => u.role === 'GUEST').length > 0 
-            ? users
-                .filter(u => u.role === 'GUEST')
-                .map((u) => <UserCard key={u.id} user={u} />)
-            : <p className='mx-5'>Немає Незареєстрованих Користувачів</p>}
+          <h1 className='font-semibold'>Unregister:</h1>
+          {isLoading
+            ? <Spiner />
+            : users.filter(u => u.role === 'GUEST').length > 0 
+              ? users
+                  .filter(u => u.role === 'GUEST')
+                  .map((u) => <UserCard key={u.id} user={u} />)
+              : <p className='mx-5'>Немає Незареєстрованих Користувачів</p>}
         </div>
-
     );
 };
 
