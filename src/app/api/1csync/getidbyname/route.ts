5b98c8d64bb46171e167fcedbb11d8689560cc29
lFobas <mishashuptar@gmail.com>
2025-10-01 20:01:21 +0300

    add client route for sync

---
+++ src/app/api/1csync/getidbyname/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function POST(request: Request) {
+  try {
+    const { searchName } = await request.json();
+    if (!searchName) {
+      return NextResponse.json({ error: "не вказано імя" }, { status: 400 });
+    }
+    const responseId = await prisma.client.findUnique({
+      where: { name: searchName },
+      select: { id: true },
+    });
+    return NextResponse.json({ id: responseId ? responseId.id : null });
+  } catch (error) {
+    console.error("Error in POST /api/1csync/getidbyname:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
